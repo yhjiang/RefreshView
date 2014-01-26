14682758ef195fe3cd14e0ea3e93138a10adeb58
yhjiang <417500972@qq.com>
2014-01-26 17:50:03 +0800

    上传代码

---
+++ 快速集成下拉刷新Tests/________Tests.m
@@ -0,0 +1,34 @@
+//
+//  ________Tests.m
+//  快速集成下拉刷新Tests
+//
+//  Created by mj on 14-1-3.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ________Tests : XCTestCase
+
+@end
+
+@implementation ________Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
